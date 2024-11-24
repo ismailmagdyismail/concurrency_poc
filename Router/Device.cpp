9886f7aa5f8861bd31ad3601e0f93401c32d276e
Magdy, Ismail (ext) (DI SW ICS MNA RD HAV1 A) <ismail00882@yahoo.com>
2024-11-24 03:12:39 +0200

    remove overhead threads added that were used to offload main thread

--- Router/Device.cpp
+++ Router/Device.cpp
@@ -20,21 +20,29 @@ void Device::acceptMessage(std::string message)
 
 void Device::start()
 {
-    std::lock_guard<std::mutex> lock(mutex);
-    if (!connected)
-    {
-        std::thread t([this]()
-                      {
-            router->connect(this);
-            mutex.lock();
-            connected = true;
-            mutex.unlock();
-            Thread::start(std::bind(&Device::someWork, this)); });
-        t.detach();
-    }
+    Thread::start(std::bind(&Device::someWork, this));
+    // std::lock_guard<std::mutex> lock(mutex);
+    // if (!connected)
+    // {
+    //     std::thread t([this]()
+    //                   {
+    //         router->connect(this);
+    //         mutex.lock();
+    //         connected = true;
+    //         mutex.unlock();
+    //         Thread::start(std::bind(&Device::someWork, this)); });
+    //     t.detach();
+    // }
 }
 void Device::someWork()
 {
+    mutex.lock();
+    if (!connected)
+    {
+        router->connect(this);
+        connected = true;
+    }
+    mutex.unlock();
     Logger::log("Device " + std::to_string(id) + " : doing work");
 }
 
